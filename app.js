f0dd3ecba8d0d6008464a03b12f9c69b0b86cdf5
Brock <brockburdine@gmail.com>
2023-01-11 09:51:09 -0600

    java

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-let str = 'Brock'
-console.log (str[str.length - 1])
+let cash = 30
+let price = 40
+
+if (cash > price) {
+    console.log("you paid extra - here's x dollars change")
+}
+else if (cash === price) {
+    console.log("you paid the exact amount, have anice day !")
+}
+else {
+    console.log("not enough money - you still owe x dollars")
+}
