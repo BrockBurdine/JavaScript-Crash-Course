d667242abcf547d755faa2af98a36f38e6b00232
Brock <brockburdine@gmail.com>
2023-01-13 09:34:46 -0600

    Loops

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-for (let i = 1; i <= 20; i++) {
-    console.log(i);
-    if (i % 3 === 0) {
-    console.log('${i} -> Frontend')
-    }
-    if (i % 5 === 0) {
-    console.log('${i} -> Simplified')
-    }
+const str = "Frontend Simplified"
 
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i])
 }
