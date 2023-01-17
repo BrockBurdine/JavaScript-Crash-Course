18bf8893ec9f4ce7b226ea7aa5edc5af9061679c
Brock <brockburdine@gmail.com>
2023-01-17 13:40:46 -0600

    Objects/Log in

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-// turn dollars into cents without using map method. Another example of whats above//
+let users = [
+{
+    username: 'Brock',
+    email: 'Brock@gmail.com',
+    password: 'Test233',
+    subscriptionStatus: 'VIP',
+    discordID: 'Brock#000',
+    lessonsCompleted: [1, 2, 3]
+},
+{
+    username: 'Brockfff',
+    email: 'Brockdfdfd@gmail.com',
+    subscriptionStatus: 'VIP',
+    discordID: 'Brockfff#000',
+    lessonsCompleted: [1, 2, 3]
+},
+{
+    username: 'Brockfffddd',
+    email: 'Brockdfdfdddd@gmail.com',
+    subscriptionStatus: 'VIP',
+    discordID: 'Brockfffddd#000',
+    lessonsCompleted: [1, 2, 3]
+},
+];
 
-let dollars = [1, 5, 10, 3];
-
-let cents = [];
-
-for (let i = 0; i < dollars.length; ++i) {
-    cents.push(dollars[i] * 100);
+function login(email, password) {
+    for (let i = 0; i < users.length; ++i) {
+        if (users[i].email === email) {
+            console.log(users[i]);
+            if (users[i].password === password) {
+                console.log('log the user in - the details are correct')
+            }
+            else {
+                console.log('password is incorrect')
+            }
+        }
+    }
 }
 
-console.log(cents);
+login('Brock@gmail.com', 'Test233');
